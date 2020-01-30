ac716bac65a1ae43299a28dab5097843634b7c59
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-01-30 11:42:45 +0530

    Added app.js,handlers,refactored the code

--- httpServer.js
+++ httpServer.js
@@ -1,20 +1,8 @@
 const {Server} = require('http');
-const {serveGuestBook, serveGuestBookPost} = require('./lib/serveGuestBookPages');
-const {serveStaticFile} = require('./lib/serveStaticFile');
-
-const findHandler = req => {
-  if (req.method === 'GET' && req.url === '/guestBook.html') return serveGuestBook;
-  if (req.method === 'POST' && req.url === '/saveComment') return serveGuestBookPost;
-  return serveStaticFile;
-};
-
-const handleConnection = (req, res) => {
-  const handler = findHandler(req);
-  handler(req, res);
-};
+const {app} = require('./lib/handlers');
 
 const main = () => {
-  const server = new Server(handleConnection);
+  const server = new Server(app.serve.bind(app));
   server.on('listening', () => console.warn('server started listening', server.address()));
   server.listen(4000);
 };
