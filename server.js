a5297d7e627ac664ff2d9692cc0b65b25e81b195
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-01-31 11:21:05 +0530

    fixed eslint errors

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const {Server} = require('http');
 const {app} = require('./lib/handlers');
 
 const main = () => {
+  const port = 4000;
   const server = new Server(app.serve.bind(app));
-  server.on('listening', () => console.warn('server started listening', server.address()));
-  server.listen(4000);
+  server.listen(port);
 };
 main();
--- test/testHandlers.js
+++ test/testHandlers.js
@@ -4,48 +4,47 @@ const {app} = require('../lib/handlers');
 describe('Get home page', () => {
   it('should get homepage on / path', done => {
     request(app.serve.bind(app))
-    .get('/')
-    .set('Accept','*/*')
-    .expect(200)
-    .expect('Content-Type','text/html',done)
-    .expect('Content-Length',"1103")
-    .expect(/Guest Book/)  
+      .get('/')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/html', done)
+      .expect(/Guest Book/);
   });
 });
 
-describe('Get non Existing url',()=>{
-  it('gives 404 for non existing url or page',(done)=>{
+describe('Get non Existing url', () => {
+  it('gives 404 for non existing url or page', done => {
     request(app.serve.bind(app))
-    .get('/badPage')
-    .expect(404,done);
-  })
-})
+      .get('/badPage')
+      .expect(404, done);
+  });
+});
 
-describe('Get guest book page',()=>{
-  it('give guest page on /guestBook.html path',(done)=>{
+describe('Get guest book page', () => {
+  it('give guest page on /guestBook.html path', done => {
     request(app.serve.bind(app))
-    .get('/guestBook.html')
-    .expect(200)
-    .expect('Content-Type','text/html',done)
-    .expect(/Leave a comment/)
-  })
-})
+      .get('/guestBook.html')
+      .expect(200)
+      .expect('Content-Type', 'text/html', done)
+      .expect(/Leave a comment/);
+  });
+});
 
-describe('serve guest book post ',()=>{
-  it('should save the comment and redirect the page',(done)=>{
+describe('serve guest book post ', () => {
+  it('should save the comment and redirect the page', done => {
     request(app.serve.bind(app))
-    .post('/saveComment')
-    .send('name=John&msg=hey dude')
-    .expect(303)
-    .expect('Location', '/guestBook.html',done)
-  })
-})
+      .post('/saveComment')
+      .send('name=John&msg=hey dude')
+      .expect(303)
+      .expect('Location', '/guestBook.html', done);
+  });
+});
 
-describe('PUT /url',()=>{
-  it('respond 400 "Method not allowed" to this request method',(done)=>{
+describe('PUT /url', () => {
+  it('respond 400 "Method not allowed" to this request method', done => {
     request(app.serve.bind(app))
-    .put('/url')
-    .expect(400)
-    .expect('Method Not Allowed',done)
-  })
-})
+      .put('/url')
+      .expect(400)
+      .expect('Method Not Allowed', done);
+  });
+});
